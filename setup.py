83aeb5f3b524f76006844a3da0590f56f26d68b0
YousofLHC <64314846+YousofLHC@users.noreply.github.com>
2025-09-06 12:39:24 +0330

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ampire",
-    version="0.1.0",
+    version="0.5.0",
     author="YousfLHC",
     author_email="",
     description="Distributed Kalman Approximate Message Passing Algorithm",
